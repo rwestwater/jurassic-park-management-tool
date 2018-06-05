8b66fad7220cb46e07b8946d576cbb48b97bfa0a
Rachel Westwater <rachel-westwater@hotmail.com>
2018-06-05 09:56:08 +0100

    Added constructors to Carnivore/Herbivore Paddocks

--- JurassicParkProject/app/src/main/java/com/example/rachelwestwater/jurassicparkproject/Paddock/CarnivorePaddock.java
+++ JurassicParkProject/app/src/main/java/com/example/rachelwestwater/jurassicparkproject/Paddock/CarnivorePaddock.java
@@ -1,4 +1,7 @@
 package com.example.rachelwestwater.jurassicparkproject.Paddock;
 
 public class CarnivorePaddock {
+    public CarnivorePaddock(String name) {
+        super(name);
+    }
 }
--- JurassicParkProject/app/src/main/java/com/example/rachelwestwater/jurassicparkproject/Paddock/HerbivorePaddock.java
+++ JurassicParkProject/app/src/main/java/com/example/rachelwestwater/jurassicparkproject/Paddock/HerbivorePaddock.java
@@ -1,4 +1,8 @@
 package com.example.rachelwestwater.jurassicparkproject.Paddock;
 
 public class HerbivorePaddock {
+    public HerbivorePaddock(String name) {
+        super(name);
+    }
 }
+
